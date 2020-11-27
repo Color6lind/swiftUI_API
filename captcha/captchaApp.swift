2dcd1a4dc324204acc008665db6bb1490ba31b7a
session1 <session1>
2020-11-27 15:26:17 +0700

    Initial Commit

---
+++ captcha/captchaApp.swift
@@ -0,0 +1,17 @@
+//
+//  captchaApp.swift
+//  captcha
+//
+//  Created by user on 27.11.2020.
+//
+
+import SwiftUI
+
+@main
+struct captchaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
